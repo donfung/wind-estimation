c8a623e762f1616b4c6afda5e9490fefc7141334
Vishnu <vishnu.vithala@joby.aero>
2021-05-31 19:28:40 -0700

    Added Process Noise Q matrix as output to wind model

--- testDrydenWindModel.m
+++ testDrydenWindModel.m
@@ -11,7 +11,7 @@ v_wind_speed=[10;1;2;1;3;4];%
 X=[1;2;3;airspeed;phi;theta;psi;1;2;3;v_wind_speed];
 for k=1:1000
    %[v_wind_dryden,A]= dryden_wind_model(airspeed,v_wind_speed(:,k),altitude,phi,theta,psi);
-   [v_wind_dryden,A]= wind_model(X);
+   [v_wind_dryden,A,Q_process]= wind_model(X);
    v_wind_speed(:,k+1)=v_wind_dryden;
 end
 plot(v_wind_speed(4,:));
@@ -19,4 +19,5 @@ hold on;
 plot(v_wind_speed(5,:));
 hold on;
 plot(v_wind_speed(6,:));
+legend('u_w','v_w','w_w')
 hold off;
